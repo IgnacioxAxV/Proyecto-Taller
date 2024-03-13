8481434eba013a877676cce2c5b5f1d58d8bdd81
Juan Ignacio <163062195+IgnacioxAxV@users.noreply.github.com>
2024-03-12 23:19:30 -0600

    codigo beta sin decoraciones

--- ARMem/Menú.py
+++ ARMem/Menú.py
@@ -18,7 +18,7 @@ def nuevoJugador(jugador:str)-> None:
     listaJugadores.append([jugador])
     return listaJugadores
 
-def menuJuego():
+"""def menuJuego():
   while True:    
     print (chr(27) + "[2J")
     print (blue)
@@ -27,10 +27,10 @@ def menuJuego():
     opt= input("Digite alguna de las opciones anteriores: ")
     if opt.lower() == "a":
        registroJugador()
-    elif opt.lower() == "b": 
+    elif opt.lower() == "b":
       validacion= len(listaJugadores)
       if validacion > 0:
-          N.primerNivel()
+          procesoJuego1(listaJugadores)
       elif validacion == 0:
           print (chr(27) + "[2J")
           print("Por favor ingrese al menos un jugador antes de empezar")
@@ -38,7 +38,7 @@ def menuJuego():
     elif opt.lower() != "a" and opt.lower() != "b" :
        print (chr(27) + "[2J")
        print("Por favor ingrese una opción válida")
-       t.sleep(2)
+       t.sleep(2)"""
       
 def registroJugador():   
   print (chr(27) + "[2J")
@@ -50,6 +50,79 @@ def registroJugador():
     else:
       nuevoJugador(jugador)
 print(listaJugadores)
+
+def procesoJuego1(listaJugadores:list):
+  N.ejecucionPrimerNivel(listaJugadores)
+  N.ejecucionSegundoNivel(listaJugadores)
+  N.ejecucionTercerNivel(listaJugadores)
+
+
+def conteoFinal(l:list):
+    cont=0
+    for i in range(len(listaJugadores)):
+        L=listaJugadores[cont]
+        report=L[1]+L[2]+L[3]
+        L.append(report)
+        cont+=1
+    quitarReportes(listaJugadores)
+    
+def quitarReportes(l:list):
+    LL= l.copy()
+    cont= 0
+    for y in range(len(listaJugadores)):
+        LL[cont].pop(1)
+        LL[cont].pop(1)
+        LL[cont].pop(1)
+        cont+=1
+    definirTop(LL)
+
+def definirTop(l:list):
+    co=0
+    while(co<len(l)-1):
+        if (l[co][1]<l[co+1][1]):
+            pass
+        else:
+            temp= l[co]
+            l[co]=l[co+1]
+            l[co+1]=temp
+        co+=1
+    imprimirTop(l)
+    
+def imprimirTop(l:list):
+    if (len(l)>=3):
+        print(f"El tercer lugar con un tiempo de {l[2][1]}s es para {l[2][0]}")
+        t.sleep(3)
+        print(f"El segundo lugar con un tiempo de {l[1][1]}s es para {l[1][0]}")
+        t.sleep(3)
+        print(f"El primer lugar con un tiempo de {l[0][1]}s es para {l[0][0]}")
+    if (len(l)==2):
+        print(f"El segundo lugar con un tiempo de {l[1][1]}s es para {l[1][0]}")
+        t.sleep(3)
+        print(f"El primer lugar con un tiempo de {l[0][1]}s es para {l[0][0]}")
+    if (len(l)==1):
+        print(f"El tiempo de {l[0][0]} fue de {l[0][1]}s")
+
+def menuJuego():
+  while True:    
+    print (chr(27) + "[2J")
+    print (blue)
+    print ("A) Para agregar un nuevo jugador")
+    print ("B) Para empezar")
+    opt= input("Digite alguna de las opciones anteriores: ")
+    if opt.lower() == "a":
+       registroJugador()
+    elif opt.lower() == "b":
+      validacion= len(listaJugadores)
+      if validacion > 0:
+          procesoJuego1(listaJugadores)
+      elif validacion == 0:
+          print (chr(27) + "[2J")
+          print("Por favor ingrese al menos un jugador antes de empezar")
+          t.sleep(2)
+    elif opt.lower() != "a" and opt.lower() != "b" :
+       print (chr(27) + "[2J")
+       print("Por favor ingrese una opción válida")
+       t.sleep(2)
+
+conteoFinal(listaJugadores)
 menuJuego()
-
-
--- ARMem/Niveles.py
+++ ARMem/Niveles.py
@@ -2,6 +2,12 @@ import time
 import ARMem as ar
 import random
 
+white= "\033[0;36m"
+red= "\033[0;31m"
+yellow= "\033[1;33m"
+black= "\033[0;30m"
+green=  "\033[0;32m"
+
 def randomizarNivel1():
     markers= [0,1,2,3,4]
     listaNivel1= []
@@ -51,7 +57,7 @@ def cicloNivel3():
     return(lis)
 
 def primerNivel():
-    #listaJuego=[[0,1,2],[4,3,0],[4,3,1],[0,4,3],[2,0,1]]
+    listaJuego=[[0,1,2],[4,3,0],[4,3,1],[0,4,3],[2,0,1]]
     listaJuego= cicloNivel1()
 
 
@@ -60,7 +66,19 @@ def primerNivel():
     tiempoTotal=0
     for x in listaJuego:
         print(f'Memorice la siguiente secuencia...')
-        print(x)
+        listaFrutas= []
+        for i in x:
+            if(i==0):
+                listaFrutas.append("Piña")
+            if(i==1):
+                listaFrutas.append("Cereza")
+            if(i==2):
+                listaFrutas.append("Uva")
+            if(i==3):
+                listaFrutas.append("Pera")
+            if(i==4):
+                listaFrutas.append("Guanabana")
+        print(listaFrutas)
         time.sleep(5)
         print(f'Ordene los marcadores en el orden que se le indicó!')
         time.sleep(3)
@@ -70,65 +88,155 @@ def primerNivel():
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
         tiempoTotal=round(tiempoTotal,2)
+
         print(f'Tiempo de partida: {tiempoPartida}s')
 
         time.sleep(7)
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 
-    print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
-    return(f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
-
+    print (f'El usuario ha completado el primer nivel en un tiempo de {tiempoTotal}s')
+    time.sleep(2)
+    return(tiempoTotal)
 
 
 def segundoNivel():
-    #listaJuego=[[0,1,2,4],[4,3,2,1],[4,2,1,0],[2,1,4,3],[3,2,0,1]]
+    listaJuego=[[0,1,2,4],[4,3,2,1],[4,2,1,0],[2,1,4,3],[3,2,0,1]]
     listaJuego= cicloNivel2()
     
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
     for x in listaJuego:
         print(f'Memorice la siguiente secuencia...')
-        print(x)
-        time.sleep(5)
+        #print(x)
+        listaFrutas= []
+        for i in x:
+            if(i==0):
+                listaFrutas.append("Piña")
+            if(i==1):
+                listaFrutas.append("Cereza")
+            if(i==2):
+                listaFrutas.append("Uva")
+            if(i==3):
+                listaFrutas.append("Pera")
+            if(i==4):
+                listaFrutas.append("Guanabana")
+        print(listaFrutas)
+        time.sleep(6)
         print(f'Ordene los marcadores en el orden que se le indicó!')
-        time.sleep(3)
+        time.sleep(4)
         
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
+        tiempoTotal=round(tiempoTotal,2)
+        print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
         print(f'Tiempo de partida: {tiempoPartida}s')
     
-        time.sleep(7)
+        time.sleep(7)     
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     
-    print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
-
+    print (f'El usuario ha completado el segundo nivel en un tiempo de {tiempoTotal}s')
+    time.sleep(2)
+    return(tiempoTotal)
 
 
 def tercerNivel():
-    #listaJuego=[[0,1,2,3,4],[4,3,2,1,0],[4,3,2,1,0],[0,2,1,4,3],[3,4,2,0,1]]
-    listaJuego= cicloNivel3()
+    listaJuego=[[0,1,2,3,4],[4,3,2,1,0],[4,3,2,1,0],[0,2,1,4,3],[3,4,2,0,1]]
+    
     
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
     for x in listaJuego:
         print(f'Memorice la siguiente secuencia...')
-        print(x)
-        time.sleep(5)
+        #print(x)
+        listaFrutas= []
+        for i in x:
+            if(i==0):
+                listaFrutas.append("Piña")
+            if(i==1):
+                listaFrutas.append("Cereza")
+            if(i==2):
+                listaFrutas.append("Uva")
+            if(i==3):
+                listaFrutas.append("Pera")
+            if(i==4):
+                listaFrutas.append("Guanabana")
+        print(listaFrutas)
+        time.sleep(7)
         print(f'Ordene los marcadores en el orden que se le indicó!')
-        time.sleep(3)
+        time.sleep(4)
         
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
+        tiempoTotal=round(tiempoTotal,2)
         print(f'Tiempo de partida: {tiempoPartida}s')
     
         time.sleep(7)
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     
-    print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
+    print (f'El usuario ha completado el tercer nivel en un tiempo de {tiempoTotal}s')
+    time.sleep(2)
+    return(tiempoTotal)
 
-#segundoNivel()
-    
+def ejecucionPrimerNivel(li:list):
+    print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
+    print(red)
+    print("Comenzando nivel 1...")
+    time.sleep(3)
+    co= 0
+    for jugador in li:
+        time.sleep(2)
+
+        print(white)
+        print("Turno de", li[co][0])
+        time.sleep(2)
+        
+        tjugador=primerNivel()
+        lis= li[co]
+        lis.insert(1,tjugador)
+        co+=1
+
+def ejecucionSegundoNivel(li:list):
+    print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
+    print(red)
+    print("Comenzando nivel 2...")
+    time.sleep(3)
+    co= 0
+    for jugador in li:
+        time.sleep(2)
+
+        print(white)
+        print("Turno de", li[co][0])
+        time.sleep(2)
+        
+        tjugador=segundoNivel()
+        lis= li[co]
+        lis.insert(2,tjugador)
+        co+=1
+
+def ejecucionTercerNivel(li:list):
+    print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
+    print(red)
+    print("Comenzando nivel 3...")
+    time.sleep(3)
+    co= 0
+    for jugador in li:
+        time.sleep(2)
+
+        print(white)
+        print("Turno de", li[co][0])
+        time.sleep(2)
+        
+        tjugador=tercerNivel()
+        lis= li[co]
+        lis.insert(3,tjugador)
+        co+=1
+
+#ejecucionPrimerNivel(jugadores)
+#ejecucionSegundoNivel(jugadores)
+#ejecucionTercerNivel(jugadores)
+
+#print(jugadores)
