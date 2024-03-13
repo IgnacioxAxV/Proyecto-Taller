65db0e4c2a068ceecaaaa12925c241f600cd40d2
WilmerA1 <163214853+WilmerA1@users.noreply.github.com>
2024-03-12 19:39:36 -0600

    Randomizador de niveles

--- ARMem/Niveles.py
+++ ARMem/Niveles.py
@@ -1,8 +1,59 @@
-import ARMem as ar
 import time
+import ARMem as ar
+import random
+
+def randomizarNivel1():
+    markers= [0,1,2,3,4]
+    listaNivel1= []
+    for x in range (3):
+        num=random.choice(markers)
+        markers.remove(num)
+        listaNivel1.append(num)
+    return(listaNivel1)
+
+def randomizarNivel2():
+    markers= [0,1,2,3,4]
+    listaNivel2= []
+    for x in range (4):
+        num=random.choice(markers)
+        markers.remove(num)
+        listaNivel2.append(num)
+    return(listaNivel2)
+
+def randomizarNivel3():
+    markers= [0,1,2,3,4]
+    listaNivel3= []
+    for x in range (5):
+        num=random.choice(markers)
+        markers.remove(num)
+        listaNivel3.append(num)
+    return(listaNivel3)
+
+def cicloNivel1():
+    lis= []
+    for i in range(5):
+        ciclos=randomizarNivel1()
+        lis.append(ciclos)
+    return(lis)
+
+def cicloNivel2():
+    lis= []
+    for i in range(5):
+        ciclos=randomizarNivel2()
+        lis.append(ciclos)
+    return(lis)
+
+def cicloNivel3():
+    lis= []
+    for i in range(5):
+        ciclos=randomizarNivel3()
+        lis.append(ciclos)
+    return(lis)
 
 def primerNivel():
     listaJuego=[[0,1,2],[4,3,0],[4,3,1],[0,4,3],[2,0,1]]
+    listaJuego= cicloNivel1()
+
 
 
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
@@ -15,20 +66,23 @@ def primerNivel():
         time.sleep(3)
 
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
+        tiempoTotal=round(tiempoTotal,2)
         print(f'Tiempo de partida: {tiempoPartida}s')
 
         time.sleep(7)
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 
     print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
-    segundoNivel()
+    return(f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
+
 
 
 def segundoNivel():
     listaJuego=[[0,1,2,4],[4,3,2,1],[4,2,1,0],[2,1,4,3],[3,2,0,1]]
-    
+    listaJuego= cicloNivel2()
     
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
@@ -49,7 +103,6 @@ def segundoNivel():
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     
     print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
-    tercerNivel()
 
 
 
@@ -77,4 +130,4 @@ def tercerNivel():
     
     print (f'El usuario ha completado el juego en un tiempo de {tiempoTotal}s')
 
-    
+segundoNivel()
