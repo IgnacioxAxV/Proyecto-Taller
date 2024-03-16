bd3503b47f6bf9f22b174866b1eb5de9e72320d5
Juan Ignacio <163062195+IgnacioxAxV@users.noreply.github.com>
2024-03-16 01:21:41 -0600

    Correción de ortografía

--- ARMem/Menú.py
+++ ARMem/Menú.py
@@ -1,63 +1,104 @@
 import time as t
 import Niveles as N
 
-black   = "\033[0;30m"
-red     = "\033[0;31m"
-green   = "\033[0;32m"
-yellow  = "\033[0;33m"
-white   = "\033[0;37m"
-nocolor = "\033[0m"
-blue    = "\033[0;34m"
+black   = "\033[0;30m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+red     = "\033[0;31m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+green   = "\033[0;32m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+yellow  = "\033[0;33m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+white   = "\033[0;37m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+nocolor = "\033[0m"         #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
+blue    = "\033[0;34m"      #Conjunto de variables utilizadas paraa cambiar el color de las fuentes de la terminal
 
-listaJugadores=[]
+listaJugadores=[['Juan', 23, 43, 32], ['Pancho', 56, 23, 43], ['Zac', 22, 10, 45], ['Lila', 27, 40, 11]]           #Lista vacía donde se colocará el nombre de jugadores y sus tiempos por nivel
+                            #correspondientes
+
 def nuevoJugador(jugador:str)-> None:
+    """Función que agrega el nombre del jugador nuevo como sublista detro de una lista predefinida.
+
+    Args:
+        jugador (str): Nombre que del jugador que se otorgue a si mismo.
+    """
     listaJugadores.append([jugador])
-    return listaJugadores
+    #return listaJugadores
       
 def registroJugador():   
-  print (chr(27) + "[2J")
-  while True:
-    jugador= input("Ingrese nombre del jugador:")
-    print("Inserte 'ok' para regresar al menu")
-    if jugador.lower() == "ok":
-      menuJuego()
-    else:
-      nuevoJugador(jugador)
-print(listaJugadores)
+    """Función que solicita y manda el nombre del usuario que se registró para si mismo en el juego 
+    a una función de registro de nombre. Además de dar la opción al usuario de regresar al menu.
+    """
+    print (chr(27) + "[2J")
+    while True:
+        jugador= input("Ingrese nombre del jugador:")
+        print("Inserte 'ok' para regresar al menu")
+        if jugador.lower() == "ok":
+            menuJuego()
+        else:
+            nuevoJugador(jugador)
+        print(listaJugadores)
 
 def conteoFinal(l:list):
+    """Función que toma todos los reportes de nivel de cada jugador y los suma para crear el reporte 
+    final del juego.
+
+    Args:
+        tiempoN (list): Referencia de la lista otorgada a la función.
+    """
     cont=0
     for i in range(len(listaJugadores)):
-        L=listaJugadores[cont]
-        reporteFinal=L[1]+L[2]+L[3]
-        L.append(reporteFinal)
+        tiempoN=listaJugadores[cont]
+        reporteFinal=tiempoN[1]+tiempoN[2]+tiempoN[3]
+        tiempoN.append(reporteFinal)
         cont+=1
     quitarReportes(listaJugadores)
-    
+
 def quitarReportes(l:list):
+    """Función que realiza una copia de la lista original que posee los nombres, reportes de nivel y de reporte
+    final y se eliminan los reportes de nivel. Para más adelante trabajar en la función de top jugadores e 
+    imprimirlos.
+
+    Args:
+        l (list): Copia de la lista origianal
+    """
     listaCopia= l.copy()
     cont= 0
     for y in range(len(listaJugadores)):
         listaCopia[cont].pop(1)
         listaCopia[cont].pop(1)
         listaCopia[cont].pop(1)
-        cont+=1
     definirTop(listaCopia)
 
 def definirTop(l:list):
-    contador=0
-    while(contador<len(l)-1):
-        if (l[contador][1]<l[contador+1][1]):
-            pass
-        else:
-            temporal= l[contador]
-            l[contador]=l[contador+1]
-            l[contador+1]=temporal
-        contador+=1
+    """Función que ordena la copia de la lista original del que tuvo el menor tiempo al que tuvo mayor tiempo
+    durante la partida del juego.
+
+    Args:
+        l (list): Referencia de la lista suministrada a la función.
+    """
+    while True:
+        cambio=False
+        for e in range(len(l)-1):
+            if (l[e][1]<l[e+1][1]):
+                pass
+            else:
+                temporal= l[e]
+                l[e]=l[e+1]
+                l[e+1]=temporal
+                cambio=True
+        if cambio == False:
+            break
+
     imprimirTop(l)
     
 def imprimirTop(l:list):
+    """Función que imprime el top de los tres mejores tiempos durante la partida. Además de uso de condicionales
+    en el caso de que la partida haya sido jugada por dos o un jugador para la impresión del top.
+
+    Args:
+        l (list):Referencia de la lista suministrada a la función. 
+    """
     print (chr(27) + "[2J")
+    print("===============")
+    print("Juego terminado")
+    print("===============")
     if (len(l)>=3):
         print(red)
         print(f"El tercer lugar con un tiempo de {l[2][1]}s es para {l[2][0]}")
@@ -77,37 +118,199 @@ def imprimirTop(l:list):
         print(f"El tiempo de {l[0][0]} fue de {l[0][1]}s")
         t.sleep(10)
 
+def reporteNivel1(l:list):
+    """Función que define el orden de los jugadores según su tiempo en 
+    el nivel 1 por medio del método de ordenamiento por burbuja.
+
+    Args:
+        l (list): lista que llega por parámetro a la función. 
+    """
+    listaCopia1=l.copy()
+    while True:
+        cambio = False
+        for e in range(len(listaCopia1)-1):
+            if (listaCopia1[e][1]<listaCopia1[e+1][1]):
+                pass
+            else :
+                temporal= l[e]
+                listaCopia1[e]=listaCopia1[e+1]
+                listaCopia1[e+1]=temporal
+                cambio=True
+        if cambio == False:
+            break
+    imprimirReporte1(listaCopia1)
+
+def imprimirReporte1(l:list):
+    """Función encargada de imprimir en la terminal los resultados de los mejores
+    tiempos de los jugadores en el nivel 1.
+
+    Args:
+        l (list): Lista que llega por parámetro a la función.
+    """
+    print (chr(27) + "[2J")
+    print("Nivel uno completado...\n")
+    if (len(l)>=3):
+        print(red)
+        print(f"El primer lugar con un tiempo de {l[0][1]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][1]}s es para {l[1][0]}")
+        print(f"El tercer lugar con un tiempo de {l[2][1]}s es para {l[2][0]}")
+        t.sleep(6)
+    if (len(l)==2):
+        print(green)
+        print(f"El primer lugar con un tiempo de {l[0][1]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][1]}s es para {l[1][0]}")
+        t.sleep(6)
+    if (len(l)==1):
+        print(yellow)
+        t.sleep(6)
+        print(f"El tiempo de {l[0][0]} fue de {l[0][1]}s")
+def reporteNivel2(l:list):
+    """Función que define el orden de los jugadores según su tiempo en 
+    el nivel 2 por medio del método de ordenamiento por burbuja.
+
+    Args:
+        l (list): lista que llega por parámetro a la función. 
+    """
+    listaCopia2=l.copy()
+    while True:
+        cambio = False
+        for e in range(len(listaCopia2)-1):
+            if (listaCopia2[e][2]<listaCopia2[e+1][2]):
+                pass
+            else :
+                temporal= listaCopia2[e]
+                listaCopia2[e]=l[e+1]
+                listaCopia2[e+1]=temporal
+                cambio=True
+        if cambio == False:
+            break
+    imprimirReporte2(listaCopia2)
+
+def imprimirReporte2(l:list):
+    """Función encargada de imprimir en la terminal los resultados de los mejores
+    tiempos de los jugadores en el nivel 1.
+
+    Args:
+        l (list): Lista que llega por parámetro a la función.
+    """
+    print (chr(27) + "[2J")
+    print("Nivel dos completado...\n")
+    if (len(l)>=3):
+        print(red)
+        print(f"El primer lugar con un tiempo de {l[0][2]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][2]}s es para {l[1][0]}")
+        print(f"El tercer lugar con un tiempo de {l[2][2]}s es para {l[2][0]}")
+        t.sleep(6)
+    if (len(l)==2):
+        print(green)
+        print(f"El primer lugar con un tiempo de {l[0][2]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][2]}s es para {l[1][0]}")
+        t.sleep(6)
+    if (len(l)==1):
+        print(yellow)
+        t.sleep(6)
+        print(f"El tiempo de {l[0][0]} fue de {l[0][2]}s")
+
+def reporteNivel3(l:list):
+    """Función que define el orden de los jugadores según su tiempo en 
+    el nivel 3 por medio del método de ordenamiento por burbuja.
+
+    Args:
+        l (list): lista que llega por parámetro a la función. 
+    """
+    listaCopia3=l.copy()
+    while True:
+        cambio = False
+        for e in range(len(listaCopia3)-1):
+            if (listaCopia3[e][3]<listaCopia3[e+1][3]):
+                pass
+            else :
+                temporal= listaCopia3[e]
+                listaCopia3[e]=l[e+1]
+                listaCopia3[e+1]=temporal
+                cambio=True
+        if cambio :
+            break
+    imprimirReporte3(listaCopia3)
+
+def imprimirReporte3(l:list):
+    """Función encargada de imprimir en la terminal los resultados de los mejores
+    tiempos de los jugadores en el nivel 1.
+
+    Args:
+        l (list): Lista que llega por parámetro a la función.
+    """
+    print (chr(27) + "[2J")
+    print("Nivel tres completado...\n")
+    if (len(l)>=3):
+        print(red)
+        print(f"El primer lugar con un tiempo de {l[0][3]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][3]}s es para {l[1][0]}")
+        print(f"El tercer lugar con un tiempo de {l[2][3]}s es para {l[2][0]}")
+        t.sleep(6)
+    if (len(l)==2):
+        print(green)
+        print(f"El primer lugar con un tiempo de {l[0][3]}s es para {l[0][0]}")
+        print(f"El segundo lugar con un tiempo de {l[1][3]}s es para {l[1][0]}")
+        t.sleep(6)
+    if (len(l)==1):
+        print(yellow)
+        print(f"El tiempo de {l[0][3]} fue de {l[0][1]}s")
+        t.sleep(6)
 
 def procesoJuego1(listaJugadores:list):
-  N.ejecucionPrimerNivel(listaJugadores)
-  N.ejecucionSegundoNivel(listaJugadores)
-  N.ejecucionTercerNivel(listaJugadores)
-  conteoFinal(listaJugadores)
-  listaJugadores.clear()
+    """Función que ejecuta cada nivel sobre la lista de jugadores ingresados al juego por medio de la 
+    importación de funciones dentro del archivo 'Niveles'. Además de llamar a las funciones encargargadas
+    de imprimir los reportes de cada nivel y final del juego.
+
+    Args:
+        listaJugadores (list): Lista de jugadores ingresados al sistema, siendo esta la original.
+    """
+    N.ejecucionPrimerNivel(listaJugadores)
+    reporteNivel1(listaJugadores)
+    N.ejecucionSegundoNivel(listaJugadores)
+    reporteNivel2(listaJugadores)
+    N.ejecucionTercerNivel(listaJugadores)
+    reporteNivel3(listaJugadores)
+    conteoFinal(listaJugadores)
+    listaJugadores.clear()
+    print("========")
+    print("Creditos")
+    print("========")
+    print("Creadores\n Wilmer Azofeifa\nIgnacio Alpízar")
+    print("¡¡¡Gracias por jugar nuestro Juego!!!")
+    print("Gracias a los profesores por el material para el proyecto")
+    menuJuego()
 
 def menuJuego():
-  while True:    
-    print (chr(27) + "[2J")
-    print (blue)
-    print ("A) Para agregar un nuevo jugador")
-    print ("B) Para empezar")
-    opcion= 0
-    opcion= input("Digite alguna de las opciones anteriores: ")
-    if opcion.lower() == "a":
-       registroJugador()
-    elif opcion.lower() == "b":
-      validacion= len(listaJugadores)
-      if validacion > 0:
-          procesoJuego1(listaJugadores)
-          menuJuego()
-      elif validacion == 0:
-          print (chr(27) + "[2J")
-          print("Por favor ingrese al menos un jugador antes de empezar")
-          t.sleep(2)
-    elif opcion.lower() != "a" and opcion.lower() != "b" :
-       print (chr(27) + "[2J")
-       print("Por favor ingrese una opción válida")
-       t.sleep(2)
-   
-#conteoFinal(listaJugadores)
-menuJuego()
+    """Función que hace papel del menu del juego, en este apartado, el usuario tiene acceso a generar nuevos jugadores
+    poder empezar el juego. 
+    """
+    while True:    
+      print (chr(27) + "[2J")
+      print (blue)
+      print ("A) Para agregar un nuevo jugador")
+      print ("B) Para empezar")
+      print ("C) Salir del Juego")
+      opcion= 0
+      opcion= input("Digite alguna de las opciones anteriores: ")
+      if opcion.lower() == "a":
+         registroJugador()
+      elif opcion.lower() == "b":
+        validacion= len(listaJugadores)   
+        if validacion > 0:
+            procesoJuego1(listaJugadores)
+            menuJuego()
+        elif validacion == 0:
+            print (chr(27) + "[2J")
+            print("Por favor ingrese al menos un jugador antes de empezar")
+            t.sleep(2)
+      elif opcion.lower() == "c":
+            break
+      elif opcion.lower() != "a" and opcion.lower() != "b"  and opcion.lower() != "c":
+         print (chr(27) + "[2J")
+         print("Por favor ingrese una opción válida")
+         t.sleep(2)
+    
+
+reporteNivel1(listaJugadores)
--- ARMem/Niveles.py
+++ ARMem/Niveles.py
@@ -13,64 +13,87 @@ morado  = "\033[0;35m"
 cian    = "\033[0;36m"
 
 def randomizarNivel1():
-    markers= [0,1,2,3,4]
+    """Función que escoge 3 números al azar de una lista definida, 
+    eliminandolos posteriormente para evitar que se repitan, retornando 
+    la lista con los números escogidos, la cual serán las iteraciones.
+    """
+    marcadores= [0,1,2,3,4]
     listaNivel1= []
     for x in range (3):
-        num=random.choice(markers)
-        markers.remove(num)
+        num=random.choice(marcadores)       #random.choice: Funcion importada que escoge un elemento al azar de una lista.
+        marcadores.remove(num)
         listaNivel1.append(num)
     return(listaNivel1)
 
 def randomizarNivel2():
-    markers= [0,1,2,3,4]
+    """Función que escoge 4 números al azar de una lista definida, 
+    eliminandolos posteriormente para evitar que se repitan, retornando 
+    la lista con los números escogidos, la cual serán las iteraciones.
+    """
+    marcadores= [0,1,2,3,4]
     listaNivel2= []
     for x in range (4):
-        num=random.choice(markers)
-        markers.remove(num)
+        num=random.choice(marcadores)       #random.choice: Funcion importada que escoge un elemento al azar de una lista.
+        marcadores.remove(num)
         listaNivel2.append(num)
     return(listaNivel2)
 
 def randomizarNivel3():
-    markers= [0,1,2,3,4]
+    """Función que escoge 5 números al azar de una lista definida, 
+    eliminandolos posteriormente para evitar que se repitan, retornando 
+    la lista con los números escogidos, la cual seran las iteraciones.
+    """
+    marcadores= [0,1,2,3,4]
     listaNivel3= []
     for x in range (5):
-        num=random.choice(markers)
-        markers.remove(num)
+        num=random.choice(marcadores)       #random.choice: Funcion importada que escoge un elemento al azar de una lista.
+        marcadores.remove(num)
         listaNivel3.append(num)
     return(listaNivel3)
 
 def cicloNivel1():
-    lis= []
+    """Función que concatena las listas retornadas por la función 
+    randomizarNivel1 para obtener las 5 iteraciones del nivel 1.
+    """
+    listaNivel1= []
     for i in range(5):
         ciclos=randomizarNivel1()
-        lis.append(ciclos)
-    return(lis)
+        listaNivel1.append(ciclos)
+    return(listaNivel1)
 
 def cicloNivel2():
-    lis= []
+    """Función que concatena las listas retornadas por la funcion 
+    randomizarNivel2 para obtener las 5 iteraciones del nivel 2.
+    """
+    listaNivel2= []
     for i in range(5):
         ciclos=randomizarNivel2()
-        lis.append(ciclos)
-    return(lis)
+        listaNivel2.append(ciclos)
+    return(listaNivel2)
 
 def cicloNivel3():
-    lis= []
+    """Función que concatena las listas retornadas por la funcion 
+    randomizarNivel3 para obtener las 5 iteraciones del nivel 3.
+    """
+    listaNivel3= []
     for i in range(5):
         ciclos=randomizarNivel3()
-        lis.append(ciclos)
-    return(lis)
+        listaNivel3.append(ciclos)
+    return(listaNivel3)
 
 def primerNivel():
-    #listaJuego=[[0,1,2],[4,3,0],[4,3,1],[0,4,3],[2,0,1]]
-    listaJuego= cicloNivel1()
-
-
-
+    """Función que con ayuda del código proporcionado para la realizacion
+    del proyecto define en que consiste el nivel 1, haciendo también un cambio
+    en la impresión para que muestre al usuario la lista con el nombre de las 
+    frutas y no la lista de numeros de cada marcador.
+    """
+    listaJuego1= cicloNivel1()
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
-    for x in listaJuego:
+    for x in listaJuego1:
         print(blue)
         print(f'Memorice la siguiente secuencia...')
+        print("\n")
         listaFrutas= []
         for i in x:
             if(i==0):
@@ -84,18 +107,15 @@ def primerNivel():
             if(i==4):
                 listaFrutas.append("Guanabana")
         print(listaFrutas)
+        print("\n")
         time.sleep(5)
         print(f'Ordene los marcadores en el orden que se le indicó!')
         time.sleep(3)
-
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
         tiempoTotal=round(tiempoTotal,2)
-
         print(f'Tiempo de partida: {tiempoPartida}s')
-
         time.sleep(7)
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     print()
@@ -103,15 +123,18 @@ def primerNivel():
     time.sleep(2)
     return(tiempoTotal)
 
-
 def segundoNivel():
-    #listaJuego=[[0,1,2,4],[4,3,2,1],[4,2,1,0],[2,1,4,3],[3,2,0,1]]
-    listaJuego= cicloNivel2()
-    
+    """Función que con ayuda del código proporcionado para la realizacion
+    del proyecto define en que consiste el nivel 2, haciendo también un cambio
+    en la impresión para que muestre al usuario la lista con el nombre de las 
+    frutas y no la lista de números de cada marcador.
+    """
+    listaJuego2= cicloNivel2()
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
-    for x in listaJuego:
+    for x in listaJuego2:
         print(f'Memorice la siguiente secuencia...')
+        print("\n")
         #print(x)
         listaFrutas= []
         for i in x:
@@ -126,34 +149,34 @@ def segundoNivel():
             if(i==4):
                 listaFrutas.append("Guanabana")
         print(listaFrutas)
+        print("\n")
         time.sleep(6)
         print(f'Ordene los marcadores en el orden que se le indicó!')
-        time.sleep(4)
-        
+        time.sleep(4) 
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-    
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
         tiempoTotal=round(tiempoTotal,2)
         print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
         print(f'Tiempo de partida: {tiempoPartida}s')
-    
-        time.sleep(7)     
+        time.sleep(7)  
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-    
     print (f'El usuario ha completado el segundo nivel en un tiempo de {tiempoTotal}s')
     time.sleep(2)
     return(tiempoTotal)
 
-
 def tercerNivel():
-    #listaJuego=[[0,1,2,3,4],[4,3,2,1,0],[4,3,2,1,0],[0,2,1,4,3],[3,4,2,0,1]]
-    listaJuego=cicloNivel3()
-    
+    """Funcion que con ayuda del codigo proporcionado para la realizacion
+    del proyecto define en que consiste el nivel 3, haciendo tambien un cambio
+    en la impresion para que muestre al usuario la lista con el nombre de las 
+    frutas y no la lista de numeros de cada marcador.
+    """
+    listaJuego3=cicloNivel3()
     print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
     tiempoTotal=0
-    for x in listaJuego:
+    for x in listaJuego3:
         print(f'Memorice la siguiente secuencia...')
+        print("\n")
         #print(x)
         listaFrutas= []
         for i in x:
@@ -168,83 +191,86 @@ def tercerNivel():
             if(i==4):
                 listaFrutas.append("Guanabana")
         print(listaFrutas)
+        print("\n")
         time.sleep(7)
         print(f'Ordene los marcadores en el orden que se le indicó!')
         time.sleep(4)
-        
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-    
         tiempoPartida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
         tiempoTotal+=tiempoPartida
         tiempoTotal=round(tiempoTotal,2)
         print(f'Tiempo de partida: {tiempoPartida}s')
-    
         time.sleep(7)
         print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-    
     print (f'El usuario ha completado el tercer nivel en un tiempo de {tiempoTotal}s')
     time.sleep(2)
     return(tiempoTotal)
 
 def ejecucionPrimerNivel(li:list):
+    """Función que llamando a la funcioón primerNivel ejecuta el nivel 1 para cada 
+    jugador que se ingrese en la lista de jugadores mediante el menu.
+
+    Args:
+        li (list): Lista de jugadores ingresada mediante el menu.
+    """
     print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
-    print(red)
+    print(green)
     print("Comenzando nivel 1...")
     time.sleep(3)
-    co= 0
+    contador= 0
     for jugador in li:
         time.sleep(2)
-
         print(white)
-        print("Turno de", li[co][0])
+        print("Turno de", li[contador][0])
         time.sleep(2)
-        
-        tjugador=primerNivel()
-        lis= li[co]
-        lis.insert(1,tjugador)
-        co+=1
+        tiempoJugador=primerNivel()
+        lis= li[contador]
+        lis.insert(1,tiempoJugador)
+        contador+=1
         time.sleep(2)
 
 def ejecucionSegundoNivel(li:list):
+    """Función que llamando a la función segundoNivel ejecuta el nivel 2 para cada 
+    jugador que se ingrese en la lista de jugadores mediante el menu.
+
+    Args:
+        li (list): Lista de jugadores ingresada mediante el menu.
+    """
     print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
-    print(red)
+    print(yellow)
     print("Comenzando nivel 2...")
     time.sleep(3)
-    co= 0
+    contador= 0
     for jugador in li:
         time.sleep(2)
-
         print(white)
-        print("Turno de", li[co][0])
+        print("Turno de", li[contador][0])
         time.sleep(2)
-        
-        tjugador=segundoNivel()
-        lis= li[co]
-        lis.insert(2,tjugador)
-        co+=1
+        tiempoJugador=segundoNivel()
+        lis= li[contador]
+        lis.insert(2,tiempoJugador)
+        contador+=1
         time.sleep(2)
 
 def ejecucionTercerNivel(li:list):
+    """Función que llamando a la función tercerNivel ejecuta el nivel 3 para cada 
+    jugador que se ingrese en la lista de jugadores mediante el menu.
+
+    Args:
+        li (list): Lista de jugadores ingresada mediante el menu.
+    """
     print('\033[2J')    # Código ANSI para limpiar la pantalla en sistemas Windows
     print(red)
     print("Comenzando nivel 3...")
     time.sleep(3)
-    co= 0
+    contador= 0
     for jugador in li:
         time.sleep(2)
-
         print(white)
-        print("Turno de", li[co][0])
+        print("Turno de", li[contador][0])
         time.sleep(2)
-        
-        tjugador=tercerNivel()
-        lis= li[co]
-        lis.insert(3,tjugador)
-        co+=1
+        tiempoJugador=tercerNivel()
+        lis= li[contador]
+        lis.insert(3,tiempoJugador)
+        contador+=1
         time.sleep(2)
-
-#ejecucionPrimerNivel(jugadores)
-#ejecucionSegundoNivel(jugadores)
-#ejecucionTercerNivel(jugadores)
-
-#print(jugadores)
